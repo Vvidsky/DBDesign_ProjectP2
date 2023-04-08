e817298451926b5d54f3a513f546db8e543bef0f
Kulawut Makkamoltham <81841604+Vvidsky@users.noreply.github.com>
2023-04-08 14:24:10 +0700

    Initialized Creation Script
    - Added Product, Product_vendor, Vendor_address, User, User_address, Payment_method, Order_item, Order_detail, Review, Review_image
    - create_at and modified_at are changed from Date to DateTime

---
+++ Database_Design - Amazon.sql
@@ -0,0 +1,121 @@
+DROP DATABASE IF EXISTS Amazon;
+CREATE DATABASE Amazon;
+USE Amazon;
+
+CREATE TABLE Product (
+	product_id 				int PRIMARY KEY,
+    product_name 			varchar(255),
+    product_description 	text, 
+    product_thumbnail		varchar(255),
+    price					decimal,
+    discount				int,
+    brand					varchar(255),
+    dimension				varchar(255),
+    created_at				datetime,
+    modified_at				datetime
+);
+
+CREATE TABLE Product_vendor(
+	vendor_id	int PRIMARY KEY,
+    vendor_name	varchar(255),
+    vendor_description varchar(255),
+    business_domain		varchar(255),
+    thumbnail_profile	varchar(255),
+    email				varchar(255),
+    phone_number		varchar(20),
+    is_verified			bool
+);
+
+CREATE TABLE Vendor_address(
+	vendor_address_id int PRIMARY KEY,
+    first_name	varchar(255),
+    last_name  varchar(255),
+    street		varchar(255),
+    city		varchar(255),
+    zipcode		varchar(255),
+    country		varchar(255),
+    home_phone	varchar(255),
+    vendor_id	int,
+    FOREIGN KEY (vendor_id) REFERENCES Product_vendor(vendor_id)
+);
+
+CREATE TABLE User(
+	user_id 		int PRIMARY KEY,
+    first_name 		varchar(255),
+    last_name		varchar(255),
+    id_prime_member	bool,
+    email			varchar(255),
+    password		varchar(255),
+    create_at		datetime,
+    modified_at		datetime,
+    pin				char(6),
+    currency		char(3),
+    birthdate		date
+);
+
+CREATE TABLE User_address(
+	user_address_id	int,
+    first_name		varchar(255),
+    last_name		varchar(255),
+    street			varchar(255),
+    city			varchar(255),
+    zipcode			varchar(255),
+    country			varchar(255),
+    mobile_phone	varchar(20),
+    user_id			int,
+    FOREIGN KEY (user_id) REFERENCES User(user_id)
+);
+
+CREATE TABLE Payment_method(
+	payment_method_id	int,
+    payment_method_name	varchar(255),
+    credit_card			varchar(19),		# Maximum number of card number length
+    user_id				int,
+    PRIMARY KEY (payment_method_id, user_id),
+    CONSTRAINT fk_payment_method_user_user_id FOREIGN KEY (user_id) REFERENCES User(user_id)
+);
+
+CREATE TABLE Order_item(
+	order_item_id	int PRIMARY KEY,
+    quantity		int,
+    price_each		double,
+    created_at		datetime,
+    modified_at		datetime
+);
+
+CREATE TABLE Order_detail(
+	order_detail_id			int PRIMARY KEY,
+    transaction_status		varchar(20),
+    created_at				datetime,
+    modified_at				datetime,
+    payment_date			datetime,
+    estimated_delivery_date	date,
+    receive_date			date,
+    comment					text,
+	shipping_fee			decimal,
+    tax_rate				decimal,
+    user_id					int,
+	payment_method_id		int,
+    CONSTRAINT chk_order_detail_tax_rate CHECK (tax_rate BETWEEN 7.5 AND 15),
+    CONSTRAINT chk_order_detail_transaction_status CHECK (transaction_status IN ('Completed', 'Pending', 'Canceled'))
+);
+
+CREATE TABLE Review(
+	review_id		 	int PRIMARY KEY,
+    rating_star			tinyint,
+    comment				text,
+    review_date			date,
+    helpful_rate_count	smallint,
+    product_id			int,
+    user_id				int,
+    CONSTRAINT fk_review_product_product_id FOREIGN KEY (product_id) REFERENCES Product(product_id),
+    CONSTRAINT fk_revice_user_user_id FOREIGN KEY (user_id) REFERENCES User(user_id)
+);
+
+CREATE TABLE Review_image(
+	review_id		int,
+    review_image	varchar(255),
+    PRIMARY KEY (review_id, review_image),
+    CONSTRAINT fk_review_image_review_id FOREIGN KEY (review_id) REFERENCES Review(review_id) 
+);
+
